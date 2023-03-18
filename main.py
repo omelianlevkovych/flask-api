7c5356f5e938ea075778e0fde69c1a0422efae64
Omelian Levkovych <ole@templafy.com>
2023-03-18 21:47:10 +0100

    Return not found when video does not exist

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
 video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video")
-video_put_args.add_argument("views", type=int, help="Views of the video")
-video_put_args.add_argument("likes", type=int, help="Likes of the video")
+video_put_args.add_argument("name", type=str, help="Name of the video is required.", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required.", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video is required.", required=True)
 
 videos = {}
 
 
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video is not found.")
+
+
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
 
-    def put(self, video_id):
+    def post(self, video_id):
         args = video_put_args.parse_args()
-        return {video_id: args}
+        videos[video_id] = args
+        return videos[video_id], 201
 
 
 api.add_resource(Video, "/video/<int:video_id>")
--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
+response = requests.post(BASE + "video/1", {"likes": 10, "views": 20, "name": "my-video"})
+response = requests.get(BASE + "video/1")
 print(response.json())
