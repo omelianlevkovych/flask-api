26801d8f3f0573402bfb58cd9bb6a3bca9fe156d
Omelian Levkovych <ole@templafy.com>
2023-03-18 20:54:45 +0100

    Add dummy hello-world GET endpoint

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World!"}
+
+
+api.add_resource(HelloWorld, "/hello-world")
+
+if __name__ == "__main__":
+    app.run(debug=True)
